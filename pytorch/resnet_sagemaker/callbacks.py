bed5bbddefbaf3b84f707fa36e921655d005da29
jbsnyder <johnbensnyder@gmail.com>
2022-03-03 18:12:34 +0000

    added utils and step time

--- pytorch/resnet_sagemaker/callbacks.py
+++ pytorch/resnet_sagemaker/callbacks.py
@@ -2,6 +2,7 @@ import os
 import pickle, gzip
 import torch
 import pytorch_lightning as pl
+from time import time
 
 class PlSageMakerLogger(pl.Callback):
     
@@ -11,8 +12,9 @@ class PlSageMakerLogger(pl.Callback):
         self.epoch = 0
     
     def on_train_epoch_start(self, trainer, module, *args, **kwargs):
-        self.inner_step = 0
+        self.inner_step = 1
         self.epoch += 1
+        self.step_time_start = time()
     
     @pl.utilities.rank_zero_only
     def on_train_batch_end(self, trainer, module, *args, **kwargs):
@@ -21,6 +23,9 @@ class PlSageMakerLogger(pl.Callback):
             print("Training Losses:")
             print(' '.join(["{0}: {1:.4f}".format(key, float(value)) \
                             for key,value in trainer.logged_metrics.items()]))
+            step_time_end = time()
+            print("Step time: {0:.2f} milliseconds".format((step_time_end - self.step_time_start)/self.frequency * 1000))
+            self.step_time_start = step_time_end
         self.inner_step += 1
         self.step += 1
         
